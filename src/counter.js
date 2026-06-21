df591eda9869b8ab247c117dcac791b5cd2b9650
TinyDeskRaves <lasers@example.com>
2026-06-21 12:28:20 +0200

    chore: clean up unnecessary debug, temp and testing files

--- src/counter.js
+++
@@ -1,9 +0,0 @@
-export function setupCounter(element) {
-  let counter = 0
-  const setCounter = (count) => {
-    counter = count
-    element.textContent = `Count is ${counter}`
-  }
-  element.addEventListener('click', () => setCounter(counter + 1))
-  setCounter(0)
-}
